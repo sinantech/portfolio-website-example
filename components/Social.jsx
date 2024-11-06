e79a2ff6254037552b227ad85fe142fc5972ee96
sinantech <sinancemozer@gmail.com>
2024-11-06 23:35:31 +0300

    feat:1.02.02 leftover

---
+++ components/Social.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaYoutube,
+  FaTwitter,
+  FaInstagram,
+  FaTwitch,
+} from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+  { icon: <FaInstagram />, path: "" },
+  { icon: <FaTwitch />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
