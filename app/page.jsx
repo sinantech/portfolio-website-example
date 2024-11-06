1398d793607191618141b4774e5794411f5b03aa
sinantech <sinancemozer@gmail.com>
2024-11-06 16:21:49 +0300

    feat:53.29

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>homepage</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div>text</div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
