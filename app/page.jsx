0d57a7be5193188d00f5ddbdc8fddc434f7e879b
sinantech <sinancemozer@gmail.com>
2024-11-07 15:47:56 +0300

    feat:1.08.24

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { FiDownload } from "react-icons/fi";
 
 //components
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I am <br />
@@ -39,7 +40,9 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
