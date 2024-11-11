4f1d3da3a1d13404fa3d7246229f093fa20fa972
sinantech <sinancemozer@gmail.com>
2024-11-11 23:56:32 +0300

    feat:1.37.53 leftover

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,65 @@
+"use client";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+//about data
+const about = {
+  title: "About Me",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aliquam corrupti tempora accusantium excepturi repellendus nemo officiis.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: " 1234567789",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "12 years",
+    },
+    {
+      fieldName: "Skype",
+      fieldValue: "luke001",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "jamaica",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "Luke@coleeman.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English",
+    },
+  ],
+};
+
+//experience data
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aliquam corrupti tempora accusantium excepturi repellendus nemo officiis.",
+};
+
 const Resume = () => {
   return <div>resume page</div>;
 };
